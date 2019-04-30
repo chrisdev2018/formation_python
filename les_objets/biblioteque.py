03a61889bdbb7bef79f57ffa9d7996ed0cf4059b
Christian FOMEKONG <christian.fomekong@gmail.com>
2019-04-30 12:30:51 +0100

    [IMP]les dictionnaires

---
+++ les_objets/biblioteque.py
@@ -0,0 +1,17 @@
+from os import *
+
+def les_chaines_de_caracteres():
+    chaine = 'BONJOUR'
+
+    chain = chaine.lower()
+
+    print(chain)
+    
+    system("pause")
+    
+def les_liste1(_liste):
+     liste =  [item for item in _liste if item[1]>item[1]]
+     
+     print(liste)
+     system("pause")
+     
---
+++ les_objets/test.py
@@ -0,0 +1,10 @@
+from biblioteque import *
+
+_dictionnaire = {
+    "permier":"tati",
+    "deuxieme":"martial",
+    "troisieme":"carole"
+}
+
+print(_dictionnaire["deuxieme"])
+system('pause')
